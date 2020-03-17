221e6a1c40bbd1e1cedcfb37ef9b6d1f0cada49d
MohanadSafwat <57914175+MohanadSafwat@users.noreply.github.com>
2020-03-17 16:18:15 +0200

    Add files via upload
    tic tac tao

--- main.cpp
+++ main.cpp
@@ -1,97 +1,185 @@
 #include <iostream>
-#include<ctime>
-#include<cstdlib>
 
 using namespace std;
-const int rows =10;
-const int elements =10;
-int matrix [rows][elements];
-int maxShips=10;
-void Clear()
+char matrix[3][3]={'1','2','3','4','5','6','7','8','9'};
+char player='X';
+void draw()
 {
- for (int i=0;i<rows;i++)
- {
-     for(int j=0;j<elements;j++)
-     {
-         matrix[i][j]=0;
-     }
- }
-}
-void show()
-{
- for (int i=0;i<rows;i++)
- {
-     for(int j=0;j<elements;j++)
-     {
-        cout<< matrix[i][j]<<" ";
-     }
-     cout<<endl;
- }
-}
-void setShips()
-{
-    int s=0;
-
-    while (s<maxShips)
-    {
-        int x =rand()%10;
-        int y =rand()%10;
-        if(matrix[x][y] !=1)
-        {
-            s++;
-            matrix[x][y]=1;
-        }
-
+    cout<<"enter a number from 1 to 9"<<endl;    // 00 01 02
+        for(int i=0;i<3;i++)                     // 10 11 12
+    {                                            // 20 21 22
+        for(int j=0;j<3;j++)
+            cout<<matrix[i][j]<<" ";
+        cout<<endl;
     }
-
-
 }
-int numberOfShips()
+void input()
 {
-    int N =0;
-   for (int i=0;i<rows;i++)
- {
-     for(int j=0;j<elements;j++)
-     {
-
-         if(matrix[i][j]==1)
-            N++;
-     }
-
- }
- return N;
+    int n;
+    cin>>n;
+    if(n==1)
+    {
+        if(matrix[0][0]=='1')
+            matrix[0][0]=player;
+        else
+        {
+            cout<<"this field is already taken "<<endl;
+            input();
+        }
+    }
+    else if(n==2)
+    {
+        if(matrix[0][1]=='2')
+            matrix[0][1]=player;
+        else
+        {
+            cout<<"this field is already taken "<<endl;
+            input();
+        }
+    }
+     else if(n==3)
+    {
+        if(matrix[0][2]=='3')
+            matrix[0][2]=player;
+        else
+        {
+            cout<<"this field is already taken "<<endl;
+            input();
+        }
+    }
+     else if(n==4)
+    {
+        if(matrix[1][0]=='4')
+            matrix[1][0]=player;
+        else
+        {
+            cout<<"this field is already taken "<<endl;
+            input();
+        }
+    }
+     else if(n==5)
+    {
+        if(matrix[1][1]=='5')
+            matrix[1][1]=player;
+        else
+        {
+            cout<<"this field is already taken "<<endl;
+            input();
+        }
+    }
+     else if(n==6)
+    {
+        if(matrix[1][2]=='6')
+            matrix[1][2]=player;
+        else
+        {
+            cout<<"this field is already taken "<<endl;
+            input();
+        }
+    }
+     else if(n==7)
+    {
+        if(matrix[2][0]=='7')
+            matrix[2][0]=player;
+        else
+        {
+            cout<<"this field is already taken "<<endl;
+            input();
+        }
+    }
+     else if(n==8)
+    {
+        if(matrix[2][1]=='8')
+            matrix[2][1]=player;
+        else
+        {
+            cout<<"this field is already taken "<<endl;
+            input();
+        }
+    }
+     else if(n==9)
+    {
+        if(matrix[2][2]=='9')
+            matrix[2][2]=player;
+        else
+        {
+            cout<<"this field is already taken "<<endl;
+            input();
+        }
+    }
 }
-bool co (int x,int y)
+void toggle ()
 {
-    if(matrix[x][y]==1)
-        {matrix[x][y]=2;
-        return true;}
-        return false;
+    if(player=='X')
+        player='O';
+    else
+    player='X';
+}
+char win()
+{
+    if(matrix[0][0]=='X'&&matrix[0][1]=='X'&&matrix[0][2]=='X')
+        return 'X';
+    if(matrix[1][0]=='X'&&matrix[1][1]=='X'&&matrix[1][2]=='X')
+        return 'X';
+    if(matrix[2][0]=='X'&&matrix[2][1]=='X'&&matrix[2][2]=='X')
+        return 'X';
+    if(matrix[0][0]=='X'&&matrix[1][0]=='X'&&matrix[2][0]=='X')
+        return 'X';
+    if(matrix[0][1]=='X'&&matrix[1][1]=='X'&&matrix[2][1]=='X')
+        return 'X';
+    if(matrix[0][2]=='X'&&matrix[1][2]=='X'&&matrix[2][2]=='X')
+        return 'X';
+    if(matrix[0][0]=='X'&&matrix[1][1]=='X'&&matrix[2][2]=='X')
+        return 'X';
+    if(matrix[0][2]=='X'&&matrix[1][1]=='X'&&matrix[2][0]=='X')
+        return 'X';
+
+
+    if(matrix[0][0]=='O'&&matrix[0][1]=='O'&&matrix[0][2]=='O')
+        return 'O';
+    if(matrix[1][0]=='O'&&matrix[1][1]=='O'&&matrix[1][2]=='O')
+        return 'O';
+    if(matrix[2][0]=='O'&&matrix[2][1]=='O'&&matrix[2][2]=='O')
+        return 'O';
+    if(matrix[0][0]=='O'&&matrix[1][0]=='O'&&matrix[2][0]=='O')
+        return 'O';
+    if(matrix[0][1]=='O'&&matrix[1][1]=='O'&&matrix[2][1]=='O')
+        return 'O';
+    if(matrix[0][2]=='O'&&matrix[1][2]=='O'&&matrix[2][2]=='O')
+        return 'O';
+    if(matrix[0][0]=='O'&&matrix[1][1]=='O'&&matrix[2][2]=='O')
+        return 'O';
+    if(matrix[0][2]=='O'&&matrix[1][1]=='O'&&matrix[2][0]=='O')
+        return 'O';
+
+    return 'D';
 }
 int main()
 {
-    srand (time(NULL));
-    char ch;
-    int pos1,pos2;
-    Clear();
-    setShips();
-    while(numberOfShips()!=0)
+    int n=0;
+    while(1)
     {
-        cout<<"enter two positions :"<<endl;
-        cin>>pos1>>pos2;
-        if(co(pos1,pos2))
-            {cout<<"congrats u got me "<<endl;
-            cout<<"number of ships is :"<<numberOfShips()<<endl;}
-        else cout<<"try again"<<endl;
-        cout<<"if u want to complete enter y else enter n "<<endl;
-        cin>>ch;
-        if(ch=='n'|| ch=='N')
+        n++;
+        draw();
+        input();
+        if(win()=='X')
         {
-            cout<<"game over"<<endl;
-            show();
+            cout<<"X wins"<<endl;
             break;
         }
+        else if (win()=='O')
+        {
+            cout<<"O wins"<<endl;
+            break;
+        }
+        else if (win()=='D'&&n==9)
+        {
+            cout<<"DRAW"<<endl;
+            break;
+        }
+        toggle();
+
     }
-    cout<<"well done dude"<<endl;
+
     return 0;
 }
